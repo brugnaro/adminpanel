4d50eb00c4786fc6b2e6177453684038a8494f3a
brugnaro <marcosrodriguesbrugnaro@gmail.com>
2017-12-02 10:45:40 -0200

    validate posts

--- app/User.php
+++ app/User.php
@@ -1,9 +1,12 @@
 <?php
-
 namespace App;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 
 class User extends Authenticatable {
-    protected $table = 'users';
+    //protected $table = 'users';
+    use \Illuminate\Auth\Authenticatable;
+    public function posts(){
+        return $this->hasMany('App\Post');
+    }
 }
